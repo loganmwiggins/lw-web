4ea967399cd35441af76530058cd8dc2399fa882
lmw20c <lmw20c@fsu.edu>
2023-10-14 00:11:19 -0400

    Bottom nav

--- lwr-scripts.js
+++ lwr-scripts.js
@@ -274,3 +274,62 @@
     }
 
 // --------------------
+
+
+
+
+
+// DATE VARS
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    switch(mm) {
+        case '1':
+            mm = "January";
+            break;
+        case '2':
+            mm = "February";
+            break;
+        case '3':
+            mm = "March";
+            break;
+        case '4':
+            mm = "April";
+            break;
+        case '5':
+            mm = "May";
+            break;
+        case '6':
+            mm = "June";
+            break;
+        case '7':
+            mm = "July";
+            break;
+        case '8':
+            mm = "August";
+            break;
+        case '9':
+            mm = "September";
+            break;
+        case '10':
+            mm = "October";
+            break;
+        case '11':
+            mm = "November";
+            break;
+        case '12':
+            mm = "December";
+            break;
+    }
+
+    today = mm + ' ' + dd + ', ' + yyyy;
+
+    window.onload = function setNavDate() {
+        let dateStr = document.getElementById("date");
+        dateStr.innerHTML = today;
+    }
+
+// --------------------
