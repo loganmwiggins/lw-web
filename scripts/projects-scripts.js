5785372306c114c752ee19e0b8b5a7db7013a08f
lmw20c <lmw20c@fsu.edu>
2024-01-21 19:21:50 -0500

    add intcovert to projects

--- scripts/projects-scripts.js
+++ scripts/projects-scripts.js
@@ -1,16 +1,16 @@
-// PROJECT CARD LINKS
+// "ALL PROJECTS" CARD LINKS
 
-    function openProjectLW() {
-        window.open("https://www.loganwiggins.com/projects-loganwiggins?", "_self");
-    }
-    function openProjectHE() {
-        window.open("https://www.loganwiggins.com/projects-holdeverything?", "_self");
-    }
-    function openProjectISO() {
-        window.open("https://www.loganwiggins.com/projects-induserv?", "_self");
-    }
-    // function openProjectGBA() {
-    //     window.open("https://www.loganwiggins.com/projects-gba?", "_self");
-    // }
+    function openProjectLW() { window.open("https://www.loganwiggins.com/projects-loganwiggins?", "_self"); }
+    function openProjectHE() { window.open("https://www.loganwiggins.com/projects-holdeverything?", "_self"); }
+    function openProjectISO() { window.open("https://www.loganwiggins.com/projects-induserv?", "_self"); }
+    function openProjectIC() { window.open("https://www.loganwiggins.com/projects-intconvert?", "_self"); }
+
+// --------------------
+
+// INTCONVERT PROJECT
+
+    function openIC() { window.open("https://www.wigginsnet.com/int-convert", "_blank"); }
+    function openICGitHub() { window.open("https://github.com/loganmwiggins/int-convert", "_blank"); }
+    function openICYouTube() { window.open("https://www.youtube.com/watch?v=-jC8TMtv1Ak&ab_channel=LoganWiggins", "_blank"); }
 
 // --------------------
